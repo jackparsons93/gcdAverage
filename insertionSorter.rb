5ea7b8187974eacb49a5a8d9d62aea61624fcf15
Chris W <jackparsons93@gmail.com>
2017-04-29 19:16:03 -0500

    Reverse insertion sort works :)

--- insertionSorter.rb
+++ insertionSorter.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
 
-
 def insertion_sort(a)
   for j in 1..(a.length - 1)
     key=a[j]
@@ -14,7 +13,6 @@ def insertion_sort(a)
   end
   return a
 end
-
 
 def reverse_insertion_sort(a)
  for j in 1..(a.length - 1)
@@ -36,20 +34,13 @@ class InsertionSortTest < Minitest::Test
     puts "this is insertion_sort test"
     arrayTopass=[1,3,2,4,2,4]
     assert insertion_sort(arrayTopass)==[1,2,2,3,4,4]
-    assert insertion_sort([3,2,1])==[1,2,3]
-  
-   
+    assert insertion_sort([3,2,1])==[1,2,3]   
   end
+
   def test_reverse_insertion_sort_will_reverse_sort
     assert reverse_insertion_sort([1,3,2,4]) == [4,3,2,1]
     assert reverse_insertion_sort([4,2,3])== [4,3,2]
   end
 end
 
-
-
-
-
-array = [1, 5, 3, 4, 6, 3]
-arrayTopass=[1,3,2,4]
 
