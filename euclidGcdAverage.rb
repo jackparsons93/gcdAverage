eb360a342c69b7637efc58447600927225553eea
Chris W <jackparsons93@gmail.com>
2017-05-03 21:09:21 -0500

    Much better syntax and refactoring to make methods concise

--- euclidGcdAverage.rb
+++ euclidGcdAverage.rb
@@ -5,15 +5,18 @@ class GCDTest < Minitest::Test
     assert gcdTestObject.euclid_gcd(20, 5)==5
     assert gcdTestObject.gcd_remainder_steps==1    
   end
+
   def test_euclid_two
     gcdTestObject=GCD.new
     assert gcdTestObject.euclid_gcd(13, 8)==1 
     assert gcdTestObject.gcd_remainder_steps==5
   end
-	def test_euclid_loop
+
+  def test_euclid_loop
     gcdTestObject=GCD.new
     assert gcdTestObject.gcd_of_5_up_to_5==[1,1,1,1,5]
-  end 
+  end
+
   def test_count_of_loop
     gcdTestObject=GCD.new
     assert gcdTestObject.remainder_steps_up_to_5==[1,2,3,2,1]
@@ -30,15 +33,15 @@ class GCD
       r= m % n
       @count+=1
     end
-		n  
+    n  
   end
 
   def gcd_remainder_steps
-  	@count
+    @count
   end
 
   def gcd_of_5_up_to_5
-  	gcdArrayUpToFive=[]
+    gcdArrayUpToFive=[]
     (1..5).map { |x| gcdArrayUpToFive << euclid_gcd(5,x) }
     gcdArrayUpToFive
   end
@@ -46,13 +49,10 @@ class GCD
   def remainder_steps_up_to_5
     gcdStepArrayUpToFive=[]
     (1..5).map do |x| 
-    	euclid_gcd(5, x)
+      euclid_gcd(5, x)
       gcdStepArrayUpToFive << gcd_remainder_steps
     end
-	gcdStepArrayUpToFive
+  gcdStepArrayUpToFive
   end
-
-def fiveLoopExtraction
-
 end
-end
+#needed comment for indentation change on stupid github
